d773dd74e92700ed1efa5d217158302f29ffe0f6
dennis.borrmann <dennis.borrmann@eecc.de>
2025-02-04 20:35:01 +0100

    added insert sql

--- delete.sql
+++ delete.sql
@@ -1,3 +1,8 @@
+/*
+    Dieses Script benutzen wir um die Datenbank zurückzusetzen.
+    Ausführen kann man es einfach per copy+paste unter https://www.eberle-zentrum.uni-tuebingen.de/lehre/db24w/_settings/?mode=plugin&func=cockpit_sql
+*/
+
 DELETE FROM Farm_Crops;
 DELETE FROM Region_Climatezone;
 DELETE FROM Measurement;
---
+++ insert.sql
@@ -0,0 +1,4 @@
+/*
+    Dieses Script benutzen wir um die Daten in die Datenbank.
+    Ausführen kann man es einfach per copy+paste unter https://www.eberle-zentrum.uni-tuebingen.de/lehre/db24w/_settings/?mode=plugin&func=cockpit_sql
+*/
